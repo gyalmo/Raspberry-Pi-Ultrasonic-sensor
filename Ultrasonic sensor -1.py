9aaf8b986db25b134a42ee6be6195fd46d88ed3c
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-30 15:20:17 +0530

    Update Ultrasonic sensor -1.py

--- Ultrasonic sensor -1.py
+++ Ultrasonic sensor -1.py
@@ -4,3 +4,19 @@ import time as t
 GPIO.setmode (GPIO.BCM)
 trigger=18
 echo=23
+GPIO.setwarnings(False)
+GPIO.setup (trigger,GPIO.OUT)
+GPIO.setup(echo,GPIO.IN)
+while True:
+   GPIO.output(trigger,True)
+   time.sleep(.0001)
+   GPIO.output(trigger,False)
+   time.sleep(1)
+   while(GPIO.input (echo)==0 ):
+      it=time.time()
+   while(GPIO.input (echo)== 1):
+      ft= time.time()
+   tt=ft-it
+   distance=(tt*34300)/2
+   print (distance)
+   time.sleep(2)
