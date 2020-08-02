660af2c9d465efa81c4f00eefdd084e72b720470
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-08-02 13:04:57 +0530

    Update Ultrasonic sensor -2.py

--- Ultrasonic sensor -2.py
+++ Ultrasonic sensor -2.py
@@ -1,1 +1,16 @@
 
+import RPi.GPIO as GPIO
+import time as t
+GPIO.setmode (GPIO.BCM)
+trigger =18 
+echo =16
+GPIO.setwarnings (False)
+GPIO.setup (trigger,GPIO.OUT)
+GPIO.setup(echo,GPIO.IN)
+while True:
+  GPIO.output(trigger,True)
+   t.sleep(0.0001)
+   GPIO.output (trigger,False)
+   t.sleep(1)
+   while(GPIO.input(echo)==0):
+      starttime = t.time
