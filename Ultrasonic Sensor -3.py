83200b607ab2080500a51029b17681afa53ecb93
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-08-03 20:36:23 +0530

    Update Ultrasonic Sensor -3.py

--- Ultrasonic Sensor -3.py
+++ Ultrasonic Sensor -3.py
@@ -1,1 +1,18 @@
 #Ultrasonic Sensor -3
+
+import RPi.GPIO as GPIO
+import time as time
+def USR():
+   GPIO.setup (16,GPIO.IN)
+   GPIO.setup (20,GPIO.OUT)
+   GPIO.setup (20,GPIO.HIGH)
+   time.sleep(0.00001)
+   GPIO.output(20,GPIO.LOW)
+    time.sleep(0.0001) 
+    while(GPIO.input(16)==0):
+        it=time.time()
+    while(GPIO.input(10)==1):
+        ft=time.time()
+    tt=ft-it
+    print (“at the distance of”,tt*34200/2)
+def IR():
